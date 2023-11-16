665051253185b362362ff5db10486e402c9aa45c
Marc Garpstål <115546127+marcgarpstl@users.noreply.github.com>
2023-11-16 12:23:04 +0100

    ReadDtos (#4)

---
+++ Measure.Domain/DTOs/ReadDTO/ReadMaleMeasuresDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Measure.Domain.DTOs.ReadDTO
+{
+    public record ReadMaleMeasuresDto(Guid Id,
+        Guid UserId,
+        int Chest,
+        int Waist,
+        int Neck,
+        int ArmLenght,
+        int Calf,
+        int Inseam,
+        int FootLength);
+
+}
---
+++ Measure.Domain/DTOs/ReadDTO/ReadUserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Measure.Domain.DTOs.ReadDTO
+{
+    public record ReadUserDto(Guid Id,
+        string FirstName,
+        string LastName,
+        string Password,
+        string UserName,
+        ICollection<ReadFemaleMeasuresDto> FemaleMeasures,
+        ICollection<ReadMaleMeasuresDto> MaleMeasures)
+    {
+        ICollection<ReadFemaleMeasuresDto> Female = new List<ReadFemaleMeasuresDto>();
+        ICollection<ReadMaleMeasuresDto> Male = new List<ReadMaleMeasuresDto>();
+    }
+}
