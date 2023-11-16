b966c372d94ec3ebdb628c1f479f5d5ed3e95fdd
marcgarpstl <115546127+marcgarpstl@users.noreply.github.com>
2023-11-16 12:18:34 +0100

    ReadDtos

---
+++ Measure.Domain/DTOs/ReadDTO/ReadFemaleMeasuresDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Measure.Domain.DTOs.ReadDTO
+{
+    public record ReadFemaleMeasuresDto(Guid Id,
+        Guid UserId,
+        int Bust,
+        int Waist,
+        int Hip,
+        int NapeWaist,
+        int FrontRise,
+        int UnderBust,
+        int MidShoulderBust,
+        int ShoulderLength,
+        int Wrist,
+        int Biceps,
+        int CrotchAnkle,
+        int Thigh,
+        int FootLength);
+}
